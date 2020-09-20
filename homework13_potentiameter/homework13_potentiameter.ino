44b8be554c53e7e7f2870aa34cc5741920b9a2cb
Guoneng Zhong <guoneng.list@gmail.com>
2020-09-20 23:19:19 +0200

    Completed homework 13

--- homework13_potentiameter/homework13_potentiameter.ino
+++ homework13_potentiameter/homework13_potentiameter.ino
@@ -1,13 +1,17 @@
 /**
- * potentiameter reading and turning on LEDs for certain values
+ * potentiameter reading and turning on three LEDs for certain values
  * https://www.youtube.com/watch?v=ORNted-NgRM
  */
 int probePin = A1;
 int baud = 9600;
 int delayTime = 200;
+int pins[3] = {2, 3, 4};
 
 void setup() {
   pinMode(probePin, INPUT);
+  for(int i=0;i<3;i++){
+    pinMode(pins[i], OUTPUT);
+  }
   Serial.begin(baud);
 }
 
@@ -16,5 +20,24 @@ void loop() {
   float voltage = (5. / 1023.) * intensity;
   Serial.print("Voltage is ");
   Serial.println(voltage);
+
+  if(voltage >4){
+    light_led(2);
+  } else if(voltage <3){
+    light_led(0);
+  } else {
+    light_led(1);
+  }
   delay(delayTime);
+}
+
+void light_led(int led_index){
+  for(int i=0; i<3;i++){
+    int pin = pins[i];
+    if(i==led_index){
+      digitalWrite(pin, HIGH);
+    } else {
+      digitalWrite(pin, LOW);
+    }
+  }
 }
