7e9d29fe21cb5b087f73f73f0c536d8ddd37a84e
Guoneng Zhong <guoneng.list@gmail.com>
2020-10-06 22:56:10 +0200

    Added comment on what the range would be

--- homework35_stepper/homework35_stepper.ino
+++ homework35_stepper/homework35_stepper.ino
@@ -9,7 +9,7 @@
 
 const int baud = 9600;
 const int stepsPR = 2048;
-int motSpeed = 10; // in RPM
+int motSpeed = 2; // in RPM from 2 to 19
 Stepper stepper(stepsPR, 8, 10, 9, 11);
 const int buttonPin = 2;
 int prevButtonState = UP;
