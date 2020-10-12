d1b6cdbcb3e41d6900a4c71d21655e1648ef1643
Guoneng Zhong <guoneng.list@gmail.com>
2020-10-12 22:46:19 +0200

    Homework 42 enhanced to use a button to toggle different states

--- homework42_shift_register_increment/homework42_shift_register_increment.ino
+++ homework42_shift_register_increment/homework42_shift_register_increment.ino
@@ -1,7 +1,21 @@
 /**
  * https://toptechboy.com/arduino-tutorial-42-understanding-how-to-use-a-serial-to-parallel-shift-register-74hc595/
- * increment the bit LEDs
+ * Also all lessons after that for the shift register
+ * There are a few buttons to change the mode
+ * 1. increment the bit LEDs
+ * 2. Shift left
+ * 3. Shift Left cyclic
+ * 4. Flip the bits
+ * They all have the same schematic
  */
+
+#define INCREMENT 0
+#define SHIFT_LEFT 1
+#define SHIFT_LEFT_CYCLIC 2
+#define FLIP 3
+
+#define UP 0
+#define DOWN 1
 
 const int baud = 9600;
 const int latchPin = 11; // latch from pin 12
@@ -9,9 +23,15 @@ const int clockPin = 9; // from pin 11
 const int serialInputPin = 12; // from pin 14
 const int delayTime = 500;
 
+// button pins
+const int controlButton = 2;
+int currentButtonState = DOWN;
+byte currentCommand = INCREMENT; 
+
 byte bits = 0x00;
 
 void setup() {
+  pinMode(controlButton, INPUT_PULLUP);
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(serialInputPin, OUTPUT);
@@ -19,11 +39,57 @@ void setup() {
 }
 
 void loop() {
+  if(buttonIsPushed()){
+    ++currentCommand;
+    currentCommand %= 4;
+    Serial.println("Button pushed to "+String(currentCommand));
+  }
+  switch(currentCommand){
+    case INCREMENT:
+      increment();
+      break;
+    case SHIFT_LEFT:
+      shiftLeft();
+      break;
+    case SHIFT_LEFT_CYCLIC:
+      shiftLeftCyclic();
+      break;
+    case FLIP:
+      flip();
+      break;
+  }
+  displayBits();
+  delay(delayTime);
+}
+
+bool buttonIsPushed(){
+  int newState = digitalRead(controlButton);
+  bool res = currentButtonState==DOWN && newState==UP;
+  currentButtonState = newState;
+  return res;
+}
+
+void shiftLeft(){
+  bits <<= 1;
+  Serial.println("shift left for "+String(bits, BIN));
+}
+void shiftLeftCyclic(){
+  bits = (bits >> 7) + (bits << 1);
+  Serial.println("shift left cyclic for "+String(bits, BIN));
+}
+void flip(){
+  Serial.println("flip for "+String(bits, BIN));
+  bits = ~bits;
+}
+void increment(){
+  Serial.println("Increment for "+String(bits, BIN));
+  bits++;
+  bits %= 0xFF;
+}
+
+void displayBits(){
   digitalWrite(latchPin, LOW);
   shiftOut(serialInputPin, clockPin, MSBFIRST, bits);
   digitalWrite(latchPin, HIGH);
   Serial.println(bits, BIN);
-  delay(delayTime);
-  bits++;
-  bits %= 0xFF;
 }
